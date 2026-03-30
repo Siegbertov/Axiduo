8d1834eba714b74773d755538db133431ac03e72
Sieg Bertov <siegbertov@gmail.com>
2026-03-30 14:59:48 +0300

    model: add enum class Question and MBTIAxis and few string-res for it

---
+++ app/src/main/java/com/s1g1/axiduo/model/MBTIAxis.kt
@@ -0,0 +1,8 @@
+package com.s1g1.axiduo.model
+
+enum class MBTIAxis(val positive: String, val negative: String){
+    Energy("E", "I"),
+    Mind("S", "N"),
+    Nature("T", "F"),
+    Tactics("J", "P"),
+}
